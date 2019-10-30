0cbb02e5389bd5e4cae7e565b3ae07a9f42e0de7
Student <student@donation.local>
2019-10-30 16:11:17 -0500

    Python Benchmark:
    Code for the python benchmark.

--- concert_tickets/app.py
+++ concert_tickets/app.py
@@ -1,17 +1,122 @@
 from datetime import datetime
+import json
+
 # additional imports if needed
 
 # global variables
-SHOWS_FILE = './shows.json'
-TRANSACTIONS_FILE = './transactions.txt'
-TICKET_FILE = './ticket.txt'
+SHOWS_FILE = "concert_tickets/shows.json"
+TRANSACTIONS_FILE = "concert_tickets/transactions.txt"
+TICKET_FILE = "concert_tickets/ticket.txt"
 SALES_TAX = 0.07  # 7% Sales Tax
 
 
+def tickets_json():
+    with open(SHOWS_FILE) as shows:
+        return json.load(shows)
+
+
+def show_concert(collection):
+    for element in collection:
+        print(
+            f"Main Artist: {element.get('artist')} | Opening Artist: {element.get('opener')} ||| Tickets: {element.get('tickets')}"
+        )
+
+
+def pick_show(collection):
+    name = input("Name: ").title()
+    print("Use the main artists name for the show!")
+    while True:
+        choice = input(">>> ").title()
+        for element in collection:
+            if choice == element.get("artist") and element.get("tickets") != "SOLD OUT":
+                return (
+                    element.get("price"),
+                    choice,
+                    name,
+                    element.get("code"),
+                    element.get("opener"),
+                    element.get("date"),
+                    element.get("doors"),
+                    element.get("show"),
+                )
+
+
+def final_transactions(choice, price, collection):
+    print(f"Price per ticket: ${price:.2f}")
+    print("How many would you like? You may only buy 4!")
+    while True:
+        tickets_sold = int(input(">>> "))
+        purchase_amount = price * tickets_sold
+        tax = purchase_amount * SALES_TAX
+        for element in collection:
+            if choice == element.get("artist") and tickets_sold <= 4:
+                available_tickets = element.get("tickets") - tickets_sold
+                return purchase_amount, tickets_sold, tax, available_tickets
+            elif tickets_sold > 4:
+                print("Exceeds ticket purchasing limit")
+                break
+
+
+def make_transaction_line(name, show, code, tickets, price, tax, timestamp):
+    return "\n{}, {}, {}, {}, {:.2f}, {:.2f} {}".format(
+        name, show, code, tickets, price, tax, timestamp
+    )
+
+
+def save_transactions(file_line):
+    with open(TRANSACTIONS_FILE, "a") as file:
+        file.write(file_line)
+
+
+def update_json(choice, available_tickets):
+    with open(SHOWS_FILE) as shows:
+        concerts = json.load(shows)
+    for idx, val in enumerate(concerts):
+        if choice == val.get("artist"):
+            concerts[idx]["tickets"] = int(available_tickets)
+            with open(SHOWS_FILE, "w") as shows:
+                json.dump(concerts, shows)
+
+
+def update_ticket(choice, opener, date, doors, show, code, tickets_sold):
+    ticket = f"""\n
+==================================================
+]                 THE JEFFERSON                  [
+]                  featuring...                  [
+]                                                [
+                 ]  {choice}[
+]                with {opener}                   [
+]                {date}                      [
+]           Doors: {doors}, Show: {show}          [
+]                                                [
+]       Admit: {tickets_sold}, Code: {code}               [
+==================================================
+"""
+    with open(TICKET_FILE, "a") as file:
+        file.write(ticket)
+
+
 def main():
-    print('Welcome to The Jefferson venue ticket purchasing tool!')
-    # this is where the code you write goes
+    print("Welcome to The Jefferson venue ticket purchasing tool!")
+    print(
+        """Only 4 tickets per person, unless show is sold out.
+    What show would you like to see?"""
+    )
+    tickets = tickets_json()
+    timestamp = datetime.now()
+    show_concert(tickets)
+    print()
+    price, choice, name, code, opener, date, doors, show = pick_show(tickets)
+    purchase_amount, tickets_sold, tax, available_tickets = final_transactions(
+        choice, price, tickets
+    )
+    transaction_line = make_transaction_line(
+        name, choice, code, tickets_sold, purchase_amount, tax, timestamp
+    )
+    save_transactions(transaction_line)
+    update_json(choice, available_tickets)
+    update_ticket(choice, opener, date, doors, show, code, tickets_sold)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- library/app.py
+++ library/app.py
@@ -1,12 +1,99 @@
 import json
+from datetime import datetime
 
-BOOK_FILENAME = "./books.json"
-LOG_FILENAME = "./log.txt"
+BOOK_FILENAME = "/Users/student/code/python_benchmark/library/books.json"
+LOG_FILENAME = "/Users/student/code/python_benchmark/library/log.txt"
+
+
+def get_books(BOOK_FILENAME):
+    with open(BOOK_FILENAME) as fiile:
+        books = json.load(fiile)
+    return books["books"]["PR6039.O32"]
+
+
+def print_books(collection):
+    print("Our books:")
+    for element in collection:
+        book_info = element.values()
+        for info in book_info:
+            print(f'{info["title"]} |In-stock: {info["in-stock"]}|')
+
+
+def choose_book(collection):
+    new_list = []
+    for book in collection:
+        info = book.values()
+        for title in info:
+            new_list.append([title["title"], title["in-stock"], book.keys()])
+    print("Which one would you like?")
+    choice = input(">>> ")
+    for items in new_list:
+        if choice == items[0] and items[1] == True:
+            number = str(items[2])
+            print("Excellent Choice")
+            return choice, number[12:15]
+    print("Sorry, that is not an option.")
+
+
+def update_checkin(collection, choice, num):
+    for element in collection:
+        for thing in element.values():
+            if choice == thing["title"]:
+                print(thing)
+                ya = thing["in-stock"] = False
+                with open(BOOK_FILENAME, "a") as book_file:
+                    json.dump(False, book_file)
+
+
+def check_out(choice, number, now):
+    with open(LOG_FILENAME, "a") as file:
+        file.write(f"\n{now},PR6039.O32 {number}, CHECK OUT, {choice}")
+
+
+def choose_checkin(collection):
+    new_list = []
+    for book in collection:
+        info = book.values()
+        for title in info:
+            new_list.append([title["title"], title["in-stock"], book.keys()])
+    print("Which one would you like?")
+    choice = input(">>> ")
+    for items in new_list:
+        if choice == items[0] and items[1] == False:
+            number = str(items[2])
+            print("Excellent Choice")
+            return choice, number[12:15]
+    print("Sorry, that is not an option.")
+
+
+def check_in(choice, number, now):
+    with open(LOG_FILENAME, "a") as file:
+        file.write(f"\n{now},PR6039.O32 {number}, CHECK IN, {choice}")
 
 
 def main():
-    print('welcome to the middle-earth public library.'.title())
+    now = datetime.now()
+    print("welcome to the middle-earth public library.".title())
+    while True:
+        action = input("Check in / Check out / Leave? [I/O/L]:")
+        if action == "O":
+            books = get_books(BOOK_FILENAME)
+            print_books(books)
+            choice, number = choose_book(books)
+            update_checkin(books, choice, number)
+            check_out(choice, number, now)
+        elif action == "I":
+            books = get_books(BOOK_FILENAME)
+            print_books(books)
+            choice, number = choose_checkin(books)
+            check_in(choice, number, now)
+            pass
+        elif action == "L":
+            print("Have a nice day!")
+            break
+        else:
+            print("That is not an option.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
